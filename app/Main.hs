3c3650411b820b2da832f2499424903262dc19d4
David van Munster <davidru01@live.nl>
2021-10-05 19:52:59 +0200

    Change model

--- app/Main.hs
+++ app/Main.hs
@@ -7,25 +7,57 @@ window :: Display
 window = InWindow "Ant Simulation" (200, 200) (10, 10)
 
 data Model = Model {
-    size :: Float,
     ants :: [Ant]
 }
 
 data Ant = Ant {
     x :: Float,
-    y :: Float
+    y :: Float,
+    size :: Float
+}
+ant :: Ant
+ant2 = Ant {
+    x = 0,
+    y = 100,
+    size = 20
+}
+ant = Ant {
+    x = 0,
+    y = 0,
+    size = 10
 }
 
 model :: Model
 model = Model {
-    size = 80
+    ants = [ant, ant2]
 }
 
 modelToPicture :: Model -> Picture
-modelToPicture model = color black $ circleSolid $ size model
+modelToPicture model = translate (x (ants model !! 0)) (y (ants model !! 0)) $ circleSolid $ size $ ants model !! 0
 
 stepIteration :: ViewPort -> Float -> Model -> Model
-stepIteration _ time model = model { size = size model + 1 }
+stepIteration _ time model = simulateModel time model 0
+--    ants = [
+--        (ants model !! 0) {
+--            --size = size (ants model !! 0) + 1,
+--            x = (x (ants model !! 0)) + 1,
+--            y = (y (ants model !! 0)) + (10 * (time * time))
+--}]}
+ 
+
+simulateModel :: Float -> Model -> Int -> Model
+simulateModel time model index = 
+    if length (ants model) <= index then
+        moveAnt 1 1 $ ants model !! index
+    else
+        moveAnt 1 1 $ ants (simulateModel time model $ index + 1) !! index
+        
+moveAnt :: Float -> Float -> Ant -> Ant
+moveAnt xO yO ant =
+    ant {
+        x = (x (ants model !! 0)) + xO,
+        y = (y (ants model !! 0)) + yO
+}
 
 main :: IO ()
-main = simulate window (greyN 0.5) 15 model modelToPicture stepIteration
+main = simulate window (greyN 0.5) 60 model modelToPicture stepIteration
