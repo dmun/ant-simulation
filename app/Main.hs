e5a3b15b142495e82ee0ad21ab3ab81fe0fb2390
David van Munster <davidru01@live.nl>
2021-10-08 00:02:53 +0200

    Fix stepIteration

--- app/Main.hs
+++ app/Main.hs
@@ -36,28 +36,13 @@ modelToPicture :: Model -> Picture
 modelToPicture model = translate (x (ants model !! 0)) (y (ants model !! 0)) $ circleSolid $ size $ ants model !! 0
 
 stepIteration :: ViewPort -> Float -> Model -> Model
-stepIteration _ time model = simulateModel time model 0
---    ants = [
---        (ants model !! 0) {
---            --size = size (ants model !! 0) + 1,
---            x = (x (ants model !! 0)) + 1,
---            y = (y (ants model !! 0)) + (10 * (time * time))
---}]}
- 
-
-simulateModel :: Float -> Model -> Int -> Model
-simulateModel time model index = 
-    if length (ants model) <= index then
-        moveAnt 1 1 $ ants model !! index
-    else
-        moveAnt 1 1 $ ants (simulateModel time model $ index + 1) !! index
-        
-moveAnt :: Float -> Float -> Ant -> Ant
-moveAnt xO yO ant =
-    ant {
-        x = (x (ants model !! 0)) + xO,
-        y = (y (ants model !! 0)) + yO
-}
+stepIteration _ time model =
+    Model { ants = ants' }
+    where
+    ants' = map (\ant -> ant {
+        x = x ant + 5,
+        y = y ant + 5
+    }) $ ants model
 
 main :: IO ()
-main = simulate window (greyN 0.5) 60 model modelToPicture stepIteration
+main = simulate window (greyN 0.5) 30 model modelToPicture stepIteration
